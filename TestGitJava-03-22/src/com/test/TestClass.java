66d8ebd65924e08cdd3b07d78d2fcb153b755aac
rajanred <rajanireddy175@gmail.com>
2022-03-03 16:50:11 +0530

    changes in 2nd commit

--- TestGitJava-03-22/src/com/test/TestClass.java
+++ TestGitJava-03-22/src/com/test/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World-22");
-		System.out.println("Hello cruel");
+		System.out.println("Hello cruel World");
 		System.out.println("Hello Futurist");
 	}
 
